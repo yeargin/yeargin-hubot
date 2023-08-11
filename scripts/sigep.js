0a0936b1509264c6366aa99c88b898690918ff88
Stephen Yeargin <stephen@yearg.in>
2023-08-11 13:56:53 -0500

    Add chapter lookup

--- scripts/sigep.js
+++ scripts/sigep.js
@@ -11,7 +11,6 @@
 const _ = require('lodash');
 
 module.exports = (robot) => {
-  // Example: Centralized request handler
   const makeAPIRequest = (method, path, payload, callback) => {
     if (method.toUpperCase() === 'GET') {
       return robot.http(`https://sigep.org/${path}`)
@@ -54,6 +53,108 @@ module.exports = (robot) => {
       robot.logger.error(parseError);
     }
   }));
+
+  // Chapter information lookup
+  robot.respond(/sigep chapter (.*)$/i, (msg) => {
+    const search = msg.match[1];
+    if (!search) {
+      msg.send('No search term provided.');
+      return;
+    }
+
+    makeAPIRequest('GET', 'wp-admin/admin-ajax.php', {
+      action: 'wp_ajax_ninja_tables_public_action',
+      table_id: 18473,
+      target_action: 'get-all-data',
+      default_sorting: 'old_first',
+    }, (err, _res, body) => {
+      if (err) {
+        robot.logger.error(err);
+        msg.send(`Error: ${err}`);
+        return;
+      }
+      try {
+        const chapters = JSON.parse(body);
+        const filteredChapter = chapters.find((row) => (
+          row.chapterdesignation.toUpperCase() === search.toUpperCase()
+            || row.dyadinstitutionalid.match(new RegExp(search, 'i'))
+        ));
+        if (filteredChapter) {
+          const fallback = `${filteredChapter.chapterdesignation} - ${filteredChapter.dyadinstitutionalid}\nManpower: ${filteredChapter.currentchaptersize} - ${filteredChapter.website}`;
+          if (robot.adapterName.includes('slack')) {
+            const { WebClient } = require('@slack/web-api');
+            const slackWebClient = new WebClient(process.env.HUBOT_SLACK_TOKEN);
+            const payload = {
+              as_user: false,
+              username: 'SigEp',
+              icon_url: 'https://sigep.org/wp-content/uploads/2016/12/cropped-Jqv6fL0f-180x180.jpg',
+              blocks: [
+                {
+                  type: 'header',
+                  text: {
+                    type: 'plain_text',
+                    text: filteredChapter.chapterdesignation,
+                  },
+                },
+                {
+                  accessory: {
+                    type: 'image',
+                    image_url: filteredChapter.logo,
+                    alt_text: filteredChapter.chapterdesignation,
+                  },
+                  type: 'section',
+                  fields: [
+                    {
+                      type: 'mrkdwn',
+                      text: `*Host Institution*\n${filteredChapter.dyadinstitutionalid}`,
+                    },
+                    {
+                      type: 'mrkdwn',
+                      text: `*Manpower*\n${filteredChapter.currentchaptersize}`,
+                    },
+                    {
+                      type: 'mrkdwn',
+                      text: `*Location*\n${filteredChapter.city}, ${filteredChapter.state}`,
+                    },
+                    {
+                      type: 'mrkdwn',
+                      text: `*Website*\n${filteredChapter.website}`,
+                    },
+                  ],
+                },
+                {
+                  type: 'divider',
+                },
+                {
+                  type: 'section',
+                  fields: [
+                    {
+                      type: 'mrkdwn',
+                      text: `*Chapter President*\n${filteredChapter.chapterpresidentname}\n${filteredChapter.chapterpresidentemail}`,
+                    },
+                    {
+                      type: 'mrkdwn',
+                      text: `*AVC President*\n${filteredChapter.avcpresidentname}\n${filteredChapter.avcpresidentemail}`,
+                    },
+                  ],
+                },
+              ],
+              channel: msg.message.room,
+              text: fallback,
+            };
+            robot.logger.debug(JSON.stringify(payload));
+            slackWebClient.chat.postMessage(payload);
+            return;
+          }
+          msg.send(fallback);
+        } else {
+          msg.send(`No chapter matched your search query: ${search}`);
+        }
+      } catch (parseError) {
+        robot.logger.error(parseError);
+      }
+    });
+  });
 
   // Total manpower count
   robot.respond(/sigep manpower$/i, (msg) => makeAPIRequest('GET', 'wp-admin/admin-ajax.php', {
