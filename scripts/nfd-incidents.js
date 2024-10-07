462239f0cb4d243278bd1b14327084f92e6422c0
Stephen Yeargin <stephen@yearg.in>
2024-10-06 20:32:55 -0500

    Update endpoint

--- scripts/nfd-incidents.js
+++ scripts/nfd-incidents.js
@@ -18,28 +18,40 @@ dayjs.tz.setDefault('America/Chicago');
 
 module.exports = (robot) => {
   // Configure dayjs
-  const baseUrl = 'https://data.nashville.gov/api/id/jwgg-8gg4.json';
+  const baseUrl = 'https://services2.arcgis.com/HdTo6HJqh92wn4D8/arcgis/rest/services/Nashville_Fire_Department_Active_Incidents_view/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=';
 
   const formatTable = (data) => {
     const table = new AsciiTable('Nashville Fire Active Incidents');
     table.setHeading('Time', 'Postal Code', 'Type', 'Unit Dispatched');
-    data.forEach((row) => {
-      table.addRow([dayjs.tz(row.dispatch_time, 'America/Chicago').fromNow(), row.postal_code, row.incident_type, row.units_dispatched]);
+    const sortedData = data.features.sort(
+      (a, b) => b.attributes.DispatchDateTime - a.attributes.DispatchDateTime,
+    );
+    sortedData.forEach((row) => {
+      const { attributes } = row;
+      table.addRow([
+        dayjs.tz(attributes.DispatchDateTime, 'America/Chicago').fromNow(),
+        attributes.PostalCode,
+        attributes.incident_type_id,
+        attributes.Unit_ID,
+      ]);
     });
     return table.toString();
   };
 
   return robot.respond(/(?:nfd|🔥|fire|:fire:)\s?(\d{5})?/i, (msg) => {
     const zip = msg.match[1];
-    let query = 'select *, :id order by `dispatch_time` desc limit 200';
+    const query = {
+      where: '1=1',
+      outFields: '*',
+      outSR: 4326,
+      f: 'json',
+    };
     if (zip) {
-      query = 'select *, :id where (`postal_code` = 37206) order by `dispatch_time` desc limit 200';
+      query.where = `PostalCode=${zip}`;
     }
 
     return robot.http(baseUrl)
-      .query({
-        $query: query,
-      })
+      .query(query)
       .get()((err, res, body) => {
         const data = JSON.parse(body);
         if (data.length === 0) {
